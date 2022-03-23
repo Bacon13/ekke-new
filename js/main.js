667759abdc562bc048223921ee2e2b0e7faae02c
Bacon <a.mosaad70@yahoo.com>
2022-03-24 00:10:58 +0200

    Uplod

---
+++ js/main.js
@@ -0,0 +1,117 @@
+let hacker = document.querySelector('.display');
+let nav = document.querySelector('#nav');
+let close = document.querySelector('#close');
+
+hacker.onclick = function () {
+    nav.classList.add('open');
+};
+close.onclick = function () {
+    this.parentElement.classList.remove('open');
+};
+
+
+$('.show').click(function (){
+
+    $('.opentxt').fadeIn();
+
+}); 
+
+$('.opentxt').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show1').click(function (){
+
+    $('.opentxt1').fadeIn();
+
+}); 
+
+$('.opentxt1').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show2').click(function (){
+
+    $('.opentxt2').fadeIn();
+
+}); 
+
+$('.opentxt2').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show3').click(function (){
+
+    $('.opentxt3').fadeIn();
+
+}); 
+
+$('.opentxt3').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show4').click(function (){
+
+    $('.opentxt4').fadeIn();
+
+}); 
+
+$('.opentxt4').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show5').click(function (){
+
+    $('.opentxt5').fadeIn();
+
+}); 
+
+$('.opentxt5').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show6').click(function (){
+
+    $('.opentxt6').fadeIn();
+
+}); 
+
+$('.opentxt6').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+$('.show7').click(function (){
+
+    $('.opentxt7').fadeIn();
+
+}); 
+
+$('.opentxt7').click(function () {
+
+    $(this).fadeOut();
+
+});
+
+
+
+
+
+
+
+
+
+
